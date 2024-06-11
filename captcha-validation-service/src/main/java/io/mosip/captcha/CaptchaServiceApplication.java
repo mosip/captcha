1e10a186b65edf5d2f22101dbf57ec7a7c797962
Yash S <119652212+yashmsonkusare@users.noreply.github.com>
2024-06-11 11:05:05 +0530

    Migration related changes
    Signed-off-by: Yash S <119652212+yashmsonkusare@users.noreply.github.com>

--- captcha-validation-service/src/main/java/io/mosip/captcha/CaptchaServiceApplication.java
+++ captcha-validation-service/src/main/java/io/mosip/captcha/CaptchaServiceApplication.java
@@ -1,10 +1,12 @@
 package io.mosip.captcha;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.jdbc.DataSourceHealthContributorAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceHealthContributorAutoConfiguration.class, DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = { "io.mosip.captcha.*", })
 public class CaptchaServiceApplication {
 
--- captcha-validation-service/src/main/java/io/mosip/captcha/config/SecurityConfiguration.java
+++ captcha-validation-service/src/main/java/io/mosip/captcha/config/SecurityConfiguration.java
@@ -3,14 +3,15 @@ package io.mosip.captcha.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
-
     @Bean
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf(httpEntry -> httpEntry.disable())
-                .authorizeRequests().requestMatchers("/").permitAll();
+    protected SecurityFilterChain configure(final HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(cfg -> cfg.disable());
+        httpSecurity.authorizeHttpRequests(
+                http -> http.anyRequest().permitAll());
+        return httpSecurity.build();
     }
 }
