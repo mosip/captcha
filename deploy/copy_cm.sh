07e3aba7665ec1dd8edb5a2e75c21efc71a41e57
Prafulrakhade <prafulrakhade02@gmail.com>
2025-12-13 16:02:59 +0530

    [MOSIP-44048] bug fix for GA release
    Signed-off-by: Prafulrakhade <prafulrakhade02@gmail.com>

--- deploy/copy_cm.sh
+++ deploy/copy_cm.sh
@@ -3,8 +3,11 @@
 # DST_NS: Destination namespace
 
 function copying_cm() {
+  UTIL_URL=https://raw.githubusercontent.com/mosip/mosip-infra/master/deployment/v3/utils/copy_cm_func.sh
   COPY_UTIL=./copy_cm_func.sh
   DST_NS=captcha
+
+  wget -q $UTIL_URL -O copy_cm_func.sh && chmod +x copy_cm_func.sh
 
   $COPY_UTIL configmap artifactory-share artifactory $DST_NS
   $COPY_UTIL configmap config-server-share config-server $DST_NS
